d4ab3e07dfd23837e6291f60141b1905289c9d3d
Libin Varghese <libinv@gmail.com>
2020-05-14 22:30:20 +0530

    chore: add scope supported by dependabot

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,6 +4,6 @@ module.exports = {
   ],
   rules: {
     'header-max-length': [2, 'always', 72],
-    'scope-enum': [2, 'always', ['yargs', 'cli']]
+    'scope-enum': [2, 'always', ['yargs', 'cli', 'deps', 'deps-dev']]
   }
 };
