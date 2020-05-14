96dc9e12841dba19539a2d8b004ed0bab5908256
Libin Varghese <libinv@gmail.com>
2020-05-14 22:24:05 +0530

    chore: setup commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional' // scoped packages are not prefixed
+  ],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'scope-enum': [2, 'always', ['yargs', 'cli']]
+  }
+};
