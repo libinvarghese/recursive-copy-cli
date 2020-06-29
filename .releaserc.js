1258418763c7cf763a3725b6d168bedeea75f4be
Libin Varghese <libinv@gmail.com>
2020-06-29 19:02:13 +0530

    ci: on release:local package.json was not updated

--- .releaserc.js
+++ .releaserc.js
@@ -43,19 +43,31 @@ All notable changes to this project will be documented in this file. See
       },
     ],
     [
+      '@semantic-release/npm',
+      {
+        pkgRoot: 'dist',
+        // Uncomment below to test semantic-release without npm
+        // npmPublish: false,
+      },
+    ],
+    // Update the package.json, since @semantic-release/npm only update the package.json in dist
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'npm --no-git-tag-version --allow-same-version version ${nextRelease.version}',
+      },
+    ],
+    [
       'semantic-release-git-branches',
       {
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
         branchMerges: ['master', 'develop'],
       },
     ],
-    ['@semantic-release/git'],
-    [
-      '@semantic-release/npm',
-      {
-        pkgRoot: 'dist',
-      },
-    ],
+    // Uncomment as alternative for git-flow
+    // ['@semantic-release/git'],
+
+    // Uncomment to test semantic-release without github actions
     [
       '@semantic-release/github',
       {
