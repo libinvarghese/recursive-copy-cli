c7569c3b33fe5806bd03591e92cd034d1d2940fe
Libin Varghese <libinv@gmail.com>
2020-07-04 03:20:12 +0530

    ci(commitlint): dependabot: fix `body-max-line-length` check

--- .commitlintrc.ci.js
+++ .commitlintrc.ci.js
@@ -3,7 +3,7 @@ const { maxLineLength } = require('@commitlint/ensure');
 module.exports = {
   extends: ['./.commitlintrc.js'],
   ignores: [
-    // Dependabot commits `chore(deps-dev): `, sometimes have `body-max-line-length` greater than 100
-    message => message.match(/^chore\(deps-dev\): /) && maxLineLength(message, 100) === false,
+    // Dependabot commits `chore(deps-dev): ` or `chore(deps): `, sometimes have `body-max-line-length` greater than 100
+    message => message.match(/^chore\(deps(-dev)?\): /) && maxLineLength(message, 100) === false,
   ],
 };
