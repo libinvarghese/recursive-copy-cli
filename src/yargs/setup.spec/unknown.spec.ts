e8f8d598bbb5715e18fd0a6b5fd78d7b9aeb3bca
Libin Varghese <libinv@gmail.com>
2020-07-06 18:31:30 +0530

    test: fix: unknown spec fail as wording changed in yargs@15.4

--- src/yargs/setup.spec/unknown.spec.ts
+++ src/yargs/setup.spec/unknown.spec.ts
@@ -7,7 +7,7 @@ import { usageRegexp } from '../../lib.spec/constants';
 describe('unknown arguments', () => {
   it('should fail when non-options arguments other than src & dest are passed', done => {
     yargs.parse('srcPath destPath someOtherArg', (error: Error, _argv: RecursiveCopyCliModel, output: unknown) => {
-      expect(error.message).to.contain('got 1');
+      expect(error.message).to.contain('Unknown');
       expect(output).to.match(usageRegexp);
       done();
     });
